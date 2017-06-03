e4087b4f9d2b4b3c7f6f1605bcf87d795471f0a4
Alexey Konovalov <kryzpoo@gmail.com>
2017-06-03 22:44:46 +0300

    Added steps

--- src/test/java/siriustest/steps/OpenPageSteps.java
+++ src/test/java/siriustest/steps/OpenPageSteps.java
@@ -12,11 +12,31 @@ public class OpenPageSteps {
 
     @Дано("^страница 'Платежи и переводы' открыта$")
     public void openedPaymentsAndTransfersPage() throws Throwable {
-        driver.get( PROPERTIES.getProperty( "page.url" ) + "/mq" );
+        driver.get( PROPERTIES.getProperty( "page.url" ) );
     }
 
     @Дано("^страница 'Главный экран' открыта$")
     public void openedMainScreenPage() throws Throwable {
-        driver.get( PROPERTIES.getProperty( "page.url" ) + "/mq/start?scenario=card_main_menu_start" );
+        driver.get( PROPERTIES.getProperty( "page.url" ) + "/start?scenario=card_main_menu_start" );
+    }
+
+    @Дано("^страница 'Платежи наличными' открыта$")
+    public void openedCashPayPage() throws Throwable {
+        driver.get( PROPERTIES.getProperty( "page.url" ) + "/start?scenario=cash_pay_start" );
+    }
+
+    @Дано("^страница 'NFC ПиП' открыта$")
+    public void openedNFCPaymentsPage() throws Throwable {
+        driver.get( PROPERTIES.getProperty( "page.url" ) + "/start?scenario=nfc_card_pay_payments_start" );
+    }
+
+    @Дано("^страница 'Самоинкассация' открыта$")
+    public void openedSAMOPage() throws Throwable {
+        driver.get( PROPERTIES.getProperty( "page.url" ) + "/start?scenario=self_encashment_start" );
+    }
+
+    @Дано("^страница 'iPAS' открыта$")
+    public void openediPASPage() throws Throwable {
+        driver.get( PROPERTIES.getProperty( "page.url" ) + "/start?scenario=card_ipas_start" );
     }
 }
